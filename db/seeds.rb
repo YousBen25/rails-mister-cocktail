0153967090ccf3f349848df6598d7611b00d588b
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-11-26 16:22:38 +0800

    MVC functionalities completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+ingredients = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+
+puts "Start creating ingredients database"
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+puts "end of procedure !!"
