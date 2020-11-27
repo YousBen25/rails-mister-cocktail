8ad92f2e3f62a9055ae4debb9f031234be908384
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-11-27 08:09:14 +0800

    search input banner + frond end new cocktail new dose

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
+  new Typed('.white-p', {
     strings: ["", "Change  your life and learn how to create the best cocktails"],
     typeSpeed: 50,
     backSpeed: 50,
