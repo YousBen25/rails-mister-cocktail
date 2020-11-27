a411409fae06f2bbd802ab4e5e8f6e85ead7f416
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2020-11-27 18:44:45 +0800

    rating validation and min 0 max 5 for input simple form

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5) }
+  validates :content, presence: true
 end
